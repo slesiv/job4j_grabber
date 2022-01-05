ccb5dfa1c81e1aff44b142b4feaf04ea0f3cba91
Sles Ilia <slesilya@mail.ru>
2022-01-06 00:11:48 +0300

    2. Парсинг HTML страницы. [#260358]

---
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -0,0 +1,17 @@
+package ru.job4j.html;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class SqlRuParse {
+    public static void main(String[] args) throws Exception {
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select(".postslisttopic");
+        for (Element tr : row) {
+            Element date = tr.parent().child(5);
+            System.out.println(date.text());
+        }
+    }
+}
