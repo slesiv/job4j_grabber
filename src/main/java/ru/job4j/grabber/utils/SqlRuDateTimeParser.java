de41f625401766c17224f24dacb89e1bbc218b9e
Sles Ilia <slesilya@mail.ru>
2022-01-06 21:15:20 +0300

    2.1. Преобразование даты [#289476]

--- src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
@@ -7,67 +7,45 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Map;
 
 public class SqlRuDateTimeParser implements DateTimeParser {
+
+    private static final Map<String, Integer> MONTHS = Map.ofEntries(
+            Map.entry("янв", 1),
+            Map.entry("фев", 2),
+            Map.entry("мар", 3),
+            Map.entry("апр", 4),
+            Map.entry("май", 5),
+            Map.entry("июн", 6),
+            Map.entry("июл", 7),
+            Map.entry("авг", 8),
+            Map.entry("сен", 9),
+            Map.entry("окт", 10),
+            Map.entry("ноя", 11),
+            Map.entry("дек", 12)
+    );
 
     @Override
     public LocalDateTime parse(String parse) {
         LocalDateTime ldt;
         String[] dateTime = parse.split(",");
         if (dateTime.length == 2 && !dateTime[0].isEmpty() && !dateTime[1].isEmpty()) {
-            ldt = LocalDateTime.of(parseDate(dateTime[0]), parseTime(dateTime[1].trim()));
+            LocalDate correctDate;
+            if ("сегодня".equals(dateTime[0])) {
+                correctDate = LocalDate.now();
+            } else if ("вчера".equals(dateTime[0])) {
+                correctDate = LocalDate.now().minusDays(1);
+            } else {
+                String[] dayMonthYear = dateTime[0].split(" ");
+                correctDate = LocalDate.of(Integer.parseInt("20" + dayMonthYear[2]), MONTHS.get(dayMonthYear[1]), Integer.parseInt(dayMonthYear[0]));
+            }
+            LocalTime correctTime = LocalTime.parse(dateTime[1].trim(), DateTimeFormatter.ofPattern("HH:mm"));
+            ldt = LocalDateTime.of(correctDate, correctTime);
         } else {
             throw new IllegalArgumentException("Получен неизвестный формат даты или времени \"dd MMM yy, HH:mm\" -> " + parse);
         }
         return ldt;
-    }
-
-    private static LocalDate parseDate(String date) {
-        String[] dayMonthYear = date.split(" ");
-        LocalDate correctDate = null;
-        try {
-            if (dayMonthYear.length == 1) {
-                if ("вчера".equals(date)) {
-                    correctDate = LocalDate.now().minusDays(1);
-                } else if ("сегодня".equals(date)) {
-                    correctDate = LocalDate.now();
-                }
-            } else if (dayMonthYear.length == 3) {
-                int month = getIntMonth(dayMonthYear[1]);
-                if (month > 0 && month <= 12) {
-                    correctDate = LocalDate.of(
-                            Integer.parseInt("20" + dayMonthYear[2]),
-                            month,
-                            Integer.parseInt(dayMonthYear[0]));
-                }
-            }
-        } catch (Exception ex) {
-            throw new IllegalArgumentException("Получен неизвестный формат даты \"dd MMM yy\" -> " + date);
-        }
-        return correctDate;
-    }
-
-    private static LocalTime parseTime(String time) {
-        LocalTime correctTime;
-        try {
-            correctTime = LocalTime.parse(time, DateTimeFormatter.ofPattern("HH:mm"));
-        } catch (Exception ex) {
-            throw new IllegalArgumentException("Получен неизвестный формат времени \"HH:mm\" -> " + time);
-        }
-        return correctTime;
-    }
-
-    private static int getIntMonth(String month) {
-        String[] ruMonths = {"январь", "февраль", "март", "апрель", "май", "июнь", "июль", "август", "сентябрь", "октябрь", "ноябрь", "декабрь"};
-        int intMonth = 0;
-        int i = 1;
-        for (String m : ruMonths) {
-            if (m.contains(month.trim().toLowerCase().substring(0, 2))) {
-                intMonth = i;
-            }
-            i++;
-        }
-        return intMonth;
     }
 
     public static void main(String[] args) throws IOException {
