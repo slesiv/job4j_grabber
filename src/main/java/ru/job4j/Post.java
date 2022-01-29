3ac34b39318c4b353f4dd8f0d3f05cd98b8b0069
Sles Ilia <slesilya@mail.ru>
2022-01-29 22:09:35 +0300

    2.2. Модель данных - Post. [#285211 #257659]

--- src/main/java/ru/job4j/Post.java
+++ src/main/java/ru/job4j/Post.java
@@ -25,12 +25,12 @@ public class Post {
 
     @Override
     public String toString() {
-        return "Post{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", link='" + link + '\'' +
-                ", description='" + description + '\'' +
-                ", created=" + created +
-                '}';
+        return "Post{"
+                + "id=" + id
+                + ", title='" + title + '\''
+                + ", link='" + link + '\''
+                + ", description='" + description + '\''
+                + ", created=" + created
+                + '}';
     }
 }
