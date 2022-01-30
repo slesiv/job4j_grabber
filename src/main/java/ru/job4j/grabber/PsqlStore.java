08efcc0b1a211fa875b0c221826dbdf6c89686c6
Sles Ilia <slesilya@mail.ru>
2022-01-30 18:36:32 +0300

    5. PsqlStore [#285209]

---
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -0,0 +1,123 @@
+package ru.job4j.grabber;
+
+import com.mchange.v2.sql.filter.FilterPreparedStatement;
+
+import java.io.FileInputStream;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class PsqlStore implements Store, AutoCloseable {
+
+    private Connection connection;
+
+    public PsqlStore(Properties properties) {
+        try {
+            Class.forName(properties.getProperty("postgres.driver"));
+            connection = DriverManager.getConnection(
+                    properties.getProperty("postgres.url"),
+                    properties.getProperty("postgres.user"),
+                    properties.getProperty("postgres.password")
+            );
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void save(Post post) {
+        try (PreparedStatement ps = connection.prepareStatement(
+                "INSERT INTO post(name, text, link, created) VALUES(?,?,?,?)")) {
+            ps.setString(1, post.getTitle());
+            ps.setString(2, post.getDescription());
+            ps.setString(3, post.getLink());
+            ps.setTimestamp(4, Timestamp.valueOf(post.getCreated()));
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Post> getAll() {
+        List<Post> posts = new ArrayList<>();
+        try (PreparedStatement ps = connection.prepareStatement(
+                "SELECT * FROM post")) {
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                posts.add(getPostFromResultSet(rs));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return posts;
+    }
+
+    @Override
+    public Post findById(int id) {
+        Post post = null;
+        try (PreparedStatement ps = connection.prepareStatement(
+                "SELECT * FROM post WHERE id = ?")) {
+            ps.setInt(1, id);
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                post = getPostFromResultSet(rs);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return post;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
+    private Post getPostFromResultSet(ResultSet rs) {
+        Post post = null;
+        try {
+             post = new Post(
+                    rs.getInt(1),
+                    rs.getString(2),
+                    rs.getString(3),
+                    rs.getString(4),
+                    rs.getTimestamp(5).toLocalDateTime()
+             );
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return post;
+    }
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        try (FileInputStream in = new FileInputStream("./src/main/resources/app.properties")) {
+            properties.load(in);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        Store store = new PsqlStore(properties);
+        Post post = new Post(
+                "Ищем Senior WebGL developer (создание 2D движка) ",
+                "Привет!\n" +
+                        "Ищу Senior Frontend разработчика для участия в создании собственного высокопроизводительного 2D движка для отрисовки графиков в сфере финтех.\n" +
+                        "\n" +
+                        "Основные технологии: TypeScript (Javascript), GLSL; Webpack; API: WebGL, Canvas, DOM; VCS: Git (Gitlab); отслеживание задач: Jira.\n" +
+                        "\n" +
+                        "Формат работы удаленный, условия официальные, много плюшек, годовой бонус до 3-х окладов.\n" +
+                        "з/п 300 000 р.\n" +
+                        "\n" +
+                        "Подробнее тут: https://telegra.ph/Senior-Frontend-developer-12-07",
+                "https://www.sql.ru/forum/1341601/ishhem-senior-webgl-developer-sozdanie-2d-dvizhka",
+                LocalDateTime.of(2022, 01, 20, 11, 44, 00)
+        );
+
+        System.out.println(store.findById(1));
+    }
+}
